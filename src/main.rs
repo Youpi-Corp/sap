64f51cd6cef37b834da5c4794343297fda9f6fa3
Bazouz660 <basile.trebus-hamann@epitech.eu>
2024-09-18 18:56:07 +0000

    [ADD]: actix web logger

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ mod infrastructure;
 mod routes;
 mod schema;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Initialize the logger with default log level set to `info`
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     let pool = db_connection::establish_connection_pool(); // Create the connection pool
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default()) // Add the Logger middleware
             .app_data(web::Data::new(pool.clone())) // Pass the pool to the app
             .configure(routes::users::init) // Initialize user-related routes
                                             //.configure(routes::posts::init) // Initialize post-related routes
