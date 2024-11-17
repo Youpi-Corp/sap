a2f130561d4950f454a0f36821625d18225d9120
Bazouz660 <basile.trebus-hamann@epitech.eu>
2024-11-17 02:44:41 +0100

    [ADD]: deployment using shuttle

--- src/db_connection.rs
+++ src/db_connection.rs
@@ -3,8 +3,7 @@ use diesel::PgConnection;
 use dotenvy::dotenv;
 use std::env;
 
-// Set up connection pool
-pub fn establish_connection_pool() -> r2d2::Pool<ConnectionManager<PgConnection>> {
+pub fn establish_connection() -> r2d2::Pool<ConnectionManager<PgConnection>> {
     dotenv().ok(); // Load environment variables from .env
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ mod infrastructure;
 mod routes;
 mod schema;
 
-use actix_web::{middleware::Logger, web, App, HttpServer};
-use env_logger::Env;
+use actix_web::{web::Data, web::ServiceConfig};
+use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::SecretStore;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
@@ -36,26 +37,23 @@ use utoipa_swagger_ui::SwaggerUi;
 )]
 struct ApiDoc;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // Initialize the logger with default log level set to `info`
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+#[shuttle_runtime::main]
+async fn main(
+    #[shuttle_runtime::Secrets] secrets: SecretStore,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let pool = db_connection::establish_connection(); // Create the connection pool
 
-    let pool = db_connection::establish_connection_pool(); // Create the connection pool
-
-    HttpServer::new(move || {
-        App::new()
-            .wrap(Logger::default()) // Add the Logger middleware
-            .app_data(web::Data::new(pool.clone())) // Pass the pool to the app
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.app_data(Data::new(pool.clone())) // Pass the pool to the app
+            .app_data(secrets.clone()) // Pass the secrets to the app
             .service(
                 SwaggerUi::new("/swagger-ui/{_:.*}")
                     .url("/api-docs/openapi.json", ApiDoc::openapi()),
             ) // Add Swagger UI
             .configure(routes::user::init) // Initialize user-related routes
             .configure(routes::info::init) // Initialize info-related routes
-            .configure(routes::auth::init) // Initialize auth-related routes
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+            .configure(routes::auth::init); // Initialize auth-related routes
+    };
+
+    Ok(config.into())
 }
