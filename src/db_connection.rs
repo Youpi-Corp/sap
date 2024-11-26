4af4c7a5dbbc7da3ad5eca45ecdfd5573a3ba3c6
Bazouz660 <basile.trebus-hamann@epitech.eu>
2024-11-26 22:59:45 +0100

    [ADD]: added connection pool limits

--- src/db_connection.rs
+++ src/db_connection.rs
@@ -4,9 +4,11 @@ use diesel::PgConnection;
 
 pub fn establish_connection() -> r2d2::Pool<ConnectionManager<PgConnection>> {
     let database_url = get_secret("DATABASE_URL").expect("DATABASE_URL must be set");
-
     let manager = ConnectionManager::<PgConnection>::new(database_url);
+
     r2d2::Pool::builder()
+        .max_size(5) // Réduire la taille maximale du pool
+        .min_idle(Some(1)) // Définir le minimum de connexions idle
         .build(manager)
         .expect("Failed to create pool.")
 }
