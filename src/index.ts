4e9bf7608b59b4c2b7a026939096980923fdb16f
Bazouz660 <basile.trebus-hamann@epitech.eu>
2025-03-23 03:20:25 +0100

    [FIX]: cors de con

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,12 @@ const app = new Elysia()
   .use(cookie())
   .use(
     cors({
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "Accept",
+      ],
       maxAge: 3600,
     })
   )
